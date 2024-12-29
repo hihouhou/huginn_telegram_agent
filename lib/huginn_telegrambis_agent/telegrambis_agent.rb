9119feae4c95479e86ceccac6fb5390d1a686d03
Nicolas Germain <ngermain@hihouhou.com>
2024-12-29 16:44:02 +0100

    poll management added

--- lib/huginn_telegrambis_agent/telegrambis_agent.rb
+++ lib/huginn_telegrambis_agent/telegrambis_agent.rb
@@ -9,6 +9,8 @@ module Agents
       <<-MD
       The Telegrambis Agent complements the very good telegram agent, it adds other interactions with the Telegram api.
 
+      The `type` can be like pinned a message for example.
+
       `chat_id` for the target channel.
 
       `message_id` is needed to select the message for possible interactions like pinned message.
@@ -33,6 +35,10 @@ module Agents
       {
         'chat_id' => '',
         'message_id' => '',
+        'question' => '',
+        'options' => '',
+        'is_anonymous' => 'false',
+        'poll_type' => 'regular',
         'debug' => 'false',
         'emit_events' => 'true',
         'expected_receive_period_in_days' => '7',
@@ -42,21 +48,41 @@ module Agents
 
     form_configurable :chat_id, type: :string
     form_configurable :message_id, type: :string
+    form_configurable :question, type: :string
+    form_configurable :options, type: :string
+    form_configurable :is_anonymous, type: :boolean
+    form_configurable :poll_type, type: :string
     form_configurable :debug, type: :boolean
     form_configurable :emit_events, type: :boolean
     form_configurable :token, type: :string
     form_configurable :expected_receive_period_in_days, type: :string
-    form_configurable :type, type: :array, values: ['pin_chat_message', 'unpin_chat_message']
+    form_configurable :type, type: :array, values: ['pin_chat_message', 'unpin_chat_message', 'send_poll', 'stop_poll']
 
     def validate_options
-      errors.add(:base, "type has invalid value: should be 'pin_chat_message', 'unpin_chat_message'") if interpolated['type'].present? && !%w(pin_chat_message unpin_chat_message).include?(interpolated['type'])
+      errors.add(:base, "type has invalid value: should be 'pin_chat_message', 'unpin_chat_message', 'send_poll', 'stop_poll'") if interpolated['type'].present? && !%w(pin_chat_message unpin_chat_message send_poll stop_poll).include?(interpolated['type'])
 
-      unless options['chat_id'].present? || !['pin_chat_message' 'unpin_chat_message'].include?(options['type'])
+      unless options['chat_id'].present? || !['pin_chat_message' 'unpin_chat_message' 'send_poll' 'stop_poll'].include?(options['type'])
         errors.add(:base, "chat_id is a required field")
       end
 
-      unless options['message_id'].present? || !['pin_chat_message' 'unpin_chat_message'].include?(options['type'])
+      unless options['message_id'].present? || !['pin_chat_message' 'unpin_chat_message' 'stop_poll'].include?(options['type'])
         errors.add(:base, "message_id is a required field")
+      end
+
+      unless options['question'].present? || !['send_poll'].include?(options['type'])
+        errors.add(:base, "question is a required field")
+      end
+
+      unless options['options'].present? || !['send_poll'].include?(options['type'])
+        errors.add(:base, "options is a required field")
+      end
+
+      unless options['is_anonymous'].present? || !['send_poll'].include?(options['type'])
+        errors.add(:base, "is_anonymous is a required field")
+      end
+
+      unless options['poll_type'].present? || !['send_poll'].include?(options['type'])
+        errors.add(:base, "poll_type is a required field")
       end
 
       if options.has_key?('debug') && boolify(options['debug']).nil?
@@ -167,6 +193,119 @@ module Agents
       end
 
     end
+#
+#    def set_message_reaction
+#
+#      log JSON.dump({
+#        "chat_id" => interpolated['chat_id'],
+#        "message_id" => interpolated['message_id'],
+#        "reaction" => [
+#          {
+#            "type" => "emoji",
+#            "emoji" => interpolated['reaction']
+#          }
+#        ]
+#      })
+#
+#
+#      uri = URI.parse("https://api.telegram.org/bot#{interpolated['token']}/setMessageReaction")
+#      request = Net::HTTP::Post.new(uri)
+#      request.content_type = "application/json"
+#      request.body = JSON.dump({
+#        "chat_id" => interpolated['chat_id'],
+#        "message_id" => interpolated['message_id'],
+#        "reaction" => [
+#          {
+#            "type" => "emoji",
+#            "emoji" => interpolated['reaction']
+#          }
+#        ]
+#      })
+#
+#      req_options = {
+#        use_ssl: uri.scheme == "https",
+#      }
+#
+#      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+#        http.request(request)
+#      end
+#
+#      log_curl_output(response.code,response.body)
+#
+#      payload = JSON.parse(response.body)
+#
+#      if interpolated['emit_events'] == 'true'
+#        payload['action'] = 'setMessageReaction'
+#        payload['chat_id'] = interpolated['chat_id']
+#        payload['message_id'] = interpolated['message_id']
+#        create_event payload: payload
+#      end
+#
+#    end
+
+    def send_poll
+
+      uri = URI.parse("https://api.telegram.org/bot#{interpolated['token']}/sendPoll")
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = "application/json"
+      request.body = JSON.dump({
+        "chat_id" => interpolated['chat_id'],
+        "question" => interpolated['question'],
+        "options" => JSON.parse(interpolated['options']),
+        "is_anonymous" => interpolated['is_anonymous'],
+        "type" => interpolated['poll_type'],
+      })
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      log_curl_output(response.code,response.body)
+
+      payload = JSON.parse(response.body)
+
+      if interpolated['emit_events'] == 'true'
+        payload['action'] = 'sendPoll'
+        payload['chat_id'] = interpolated['chat_id']
+        create_event payload: payload
+      end
+
+    end
+
+    def stop_poll
+
+      uri = URI.parse("https://api.telegram.org/bot#{interpolated['token']}/stopPoll")
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = "application/json"
+      request.body = JSON.dump({
+        "chat_id" => interpolated['chat_id'],
+        "message_id" => interpolated['message_id']
+      })
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      log_curl_output(response.code,response.body)
+
+      payload = JSON.parse(response.body)
+
+      if interpolated['emit_events'] == 'true'
+        payload['action'] = 'stopPoll'
+        payload['chat_id'] = interpolated['chat_id']
+        payload['message_id'] = interpolated['message_id']
+        create_event payload: payload
+      end
+
+    end
 
     def trigger_action
 
@@ -175,6 +314,12 @@ module Agents
         pin_chat_message()
       when "unpin_chat_message"
         unpin_chat_message()
+#      when "set_message_reaction"
+#        set_message_reaction()
+      when "send_poll"
+        send_poll()
+      when "stop_poll"
+        stop_poll()
       else
         log "Error: type has an invalid value (#{interpolated['type']})"
       end
